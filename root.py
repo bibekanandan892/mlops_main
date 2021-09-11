7f6e26e42dddfd711e12c2253af79ce79946280e
bibekananda nayak <bibekanandan892@gmail.com>
2021-09-11 18:31:36 +0530

    frist commit

--- root.py
+++ root.py
@@ -7,11 +7,14 @@ data_dirs = ["Training_Batch_Files" , "Prediction_Batch_files"]
 
 for data_dir in data_dirs:
     files = glob(data_dir + "/*.csv")
-    print(files)
+    # print(files)
     for filePath in files:
-        print(filePath)
+        # print(filePath)
 
-        # print(f"dvc add {filePath}")
+        print(f"dvc add {filePath}")
+        os.system(f"git rm -r --cached {filePath}")
+        stop='stop tracking '
+        os.system(f"git commit -m {stop} {filePath}")
         os.system(f"dvc add {filePath}")
 
 print("\n #### all files added to dvc ####\ndvc")
